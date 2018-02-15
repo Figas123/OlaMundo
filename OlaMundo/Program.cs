1184aac84d49dd7c80d0c9db7354da21081fd240
Nuno <nunosilvares@hotmail.com>
2018-02-15 11:32:10 +0000

    Adicionar uma coisa para ler os argumentos

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -10,6 +10,10 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
             Console.WriteLine("Ola Mundo!");
             Console.ReadKey();
         }
